32f93065ab1aa459249bc08795e81bcd244aea14
Guille <guillearg@gmail.com>
2020-08-03 03:34:09 -0300

    export to json file

--- walmart.js
+++ walmart.js
@@ -1,5 +1,5 @@
 let url = 'https://www.walmart.com/browse/home/dorm-room-essentials/4044_1225301_1225229_7506721';
-
+// let lastPage = Array.from(document.querySelectorAll("ul.paginator-list>li")).map(each => each.textContent)
 
 const puppeteer = require('puppeteer');
 const fs = require('fs');
@@ -15,9 +15,9 @@ var data = [];
         // get desc product on the page
         // const categories = await page.evaluate(() => (Array.from(document.querySelectorAll("a.category__link")).map((each) => each.href)));
 
-        const desc = await page.evaluate(() => (Array.from(document.querySelectorAll("a.product-title-link.line-clamp.line-clamp-2.truncate-title")).map(each => each.text)));
+        let desc = await page.evaluate(() => (Array.from(document.querySelectorAll("a.product-title-link.line-clamp.line-clamp-2.truncate-title")).map(each => each.text)));
 
-        const price = await page.evaluate(() => (Array.from(document.querySelectorAll("div.product-price-with-fulfillment")).map(each => each.outerText.split('-')[0].split('$')[1].substring(0, each.outerText.split('-')[0].split('$')[1].length - 1))));
+        let price = await page.evaluate(() => (Array.from(document.querySelectorAll("div.product-price-with-fulfillment")).map(each => each.outerText.split('-')[0].split('$')[1].substring(0, each.outerText.split('-')[0].split('$')[1].length - 1))));
 
         // const idnumber = await page.evaluate(() => (Array.from(document.querySelectorAll("div.buy-button-normal")).map(each => each.id)));
 
@@ -30,6 +30,29 @@ var data = [];
             data.push(element);
 
         }
+
+
+        let lastPage = await page.evaluate(() => (Array.from(document.querySelectorAll("ul.paginator-list>li")).map(each => each.textContent)));
+        // console.logNumber(lastPage[lastPage.length - 1])
+        for (let i = 2; i <= lastPage[lastPage.length - 1]; i++) {
+            desc = await page.evaluate(() => (Array.from(document.querySelectorAll("a.product-title-link.line-clamp.line-clamp-2.truncate-title")).map(each => each.text)));
+
+            price = await page.evaluate(() => (Array.from(document.querySelectorAll("div.product-price-with-fulfillment")).map(each => each.outerText.split('-')[0].split('$')[1].substring(0, each.outerText.split('-')[0].split('$')[1].length - 1))));
+
+            // const idnumber = await page.evaluate(() => (Array.from(document.querySelectorAll("div.buy-button-normal")).map(each => each.id)));
+
+            for (var y = 0; y < desc.length; y++) {
+                element = {
+                    "product": desc[y],
+                    "price": price[y]
+                };
+                data.push(element);
+
+            }
+
+        }
+
+
         // await page.click('[name="commit"]')
         // console.log(data);
         return data;
@@ -38,8 +61,17 @@ var data = [];
     }
     const browser = await puppeteer.launch({ headless: true });
 
-    console.log(await getProducts(url));
+    let lista = await getProducts(url);
     // console.log(data)
+    const data = JSON.stringify(lista);
+
+    // write JSON string to a file
+    await fs.writeFile('user.json', data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+    });
     await browser.close();
 })();
 
